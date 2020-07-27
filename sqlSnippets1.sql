bbb7355dfa7d1ebee71052c74e6a0e0bd0825b24
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-07-27 11:40:08 -0400

    Update sqlSnippets1.sql

--- sqlSnippets1.sql
+++ sqlSnippets1.sql
@@ -1,3 +1,11 @@
 select * from students;
 insert into students (studentname, programid) values ('Mary', 'MAD');
+insert into students (studentname, programid) values ('John', 'CSAT');
+insert into students (studentname, programid) values ('Susan', 'CPCT');
+insert into students (studentname, programid) values ('JOE', 'MAD');
 select * from students;
+
+insert into classes(coursename, datetime, instructorid, roomid) values ('JAVA', 'MON_AM','JENELLE', 'B105');
+insert into classes(coursename, datetime, instructorid, roomid) values ('EMERG TECH', 'WED_PM','AMIR', 'C106');
+insert into classes(coursename, datetime, instructorid, roomid) values ('MATH', 'FRI_AM','PETER', 'A506');
+select * from classes;
